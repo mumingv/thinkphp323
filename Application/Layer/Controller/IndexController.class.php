254449fc2bda463bf7ea7368fac87e3a52046f07
mumingv <mumingv@163.com>
2016-06-12 00:08:48 +0800

    学习Layer示例

--- Application/Layer/Controller/IndexController.class.php
+++ Application/Layer/Controller/IndexController.class.php
@@ -10,14 +10,23 @@
  *      History:
  ***********************************************************/
 
-/**
- * http://123.56.21.232:8082/index.php?m=layer
- */
-
 namespace Layer\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+    /**
+     * 示例：http://layer.layui.com/hello.html
+     * http://123.56.21.232:8082/index.php?m=layer
+     * http://123.56.21.232:8082/index.php?m=layer&c=index&a=index
+     */
     public function index(){
         $this->display(); //访问页面Application/Layer/View/Index/index.html
     }
+
+    /**
+     * http://123.56.21.232:8082/index.php?m=layer&c=index&a=demo
+     */
+    public function demo(){
+        $this->display(); //访问页面Application/Layer/View/Index/demo.html
+    }
+
 }
